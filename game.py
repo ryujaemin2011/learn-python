3231a1da0f88ba0ae0bff2fd6fa7dff96d7ddd2a
유재민 <ryujaemin2011@gmail.com>
2025-05-05 16:19:47 +0900

    출력문구 친절하게 바꿨음

--- game.py
+++ game.py
@@ -5,12 +5,13 @@ while True: #무한반복
     b = random.randint(1, 10) #1~10 난수 b에 저장
     c = input(str(a) + " + " + str(b) + " = ") #질문, 답변받기
     if not c.isdigit(): #이상한거 입력했을떄
-        print("err") #에러문구 출력
+        print("숫자를 입력하세요") #에러문구 출력
         continue #4번줄로 돌아가기
     if int(c) == a + b: #정답일때
-        print("O") #O 출력
+        print("정답") #'정답' 출력
         count += 1 #카운트 +1
     else: #틀렸을때
-        print("x") #X 출력
-        print(count) #맞힌갯수 출력
+        print("오답") #'오답' 출력
+        print("정답 : " + str(a + b)) #정답 알려줌
+        print("맞힌갯수 : " + str(count)) #맞힌갯수 출력
         break #종료
