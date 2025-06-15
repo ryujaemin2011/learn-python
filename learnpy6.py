f2e14beeead161ff33765c9a2672de7c0582efe1
유재민 <ryujaemin2011@gmail.com>
2025-06-15 15:05:55 +0900

    learn

---
+++ learnpy6.py
@@ -0,0 +1,32 @@
+a = 123 #정수 자료형(int)
+a = -3.45 #tlftn wkfygud(float)
+a = 4.24E10 #컴퓨터식 지수 표현
+a = 0o177 #8진수 0o177 = 1x8^2 +7x8^1 + 7 = 127
+a = 0xABC #16진수 0x8ff = 10x16^2 + 11x16^1 + 12 = 2748(A:10, B:11, C:12)
+a = 3
+b = 4
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b) #사칙 연산
+print(a ** b) #x의 y제곱
+print(a % b) #나머지 연산자
+print(a // b) #몫 연산자
+a = 1
+a = a + 1
+print(a)
+a = 1
+a+= 1
+print(a)
+a -= 1
+print(a)
+a *= 20
+print(a)
+a /= 2
+print(a)
+a //= 2
+print(a)
+a %= 3
+print(a)
+a **= 2
+print(a) #복합 연산자
