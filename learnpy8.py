18c8e40296078955dc1f6f4f34cb69d4659946f6
유재민 <ryujaemin2011@gmail.com>
2025-06-15 15:41:48 +0900

    learn

---
+++ learnpy8.py
@@ -0,0 +1,14 @@
+"""
+파이썬의 재미있는 기능
+(다른 언어에선 쉽게 찾아 볼 수 없음)
+"문자열 계산"
+"""
+head = "Python"
+tail = " is fun!"
+print(head + tail)
+a = "python"
+print(a * 2)
+"""활용"""
+print("=" * 50)
+print("My Program")
+print("=" * 50)
