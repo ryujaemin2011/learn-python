6c669dd4d1db935de28eddb6f7edceda03de4c3b
유재민 <ryujaemin2011@gmail.com>
2025-07-06 16:39:37 +0900

    learn

---
+++ learn/learnpy17.py
@@ -0,0 +1,21 @@
+#맷플롯립
+import matplotlib.pyplot as plt
+
+#데이터 준비
+data = [11, 14, 12, 9, 15, 17]
+
+#꺽은선그래프 정의
+plt.plot(data)
+
+#그리기
+plt.show()
+
+#x,y 데이터 준비
+x = [7,8,9,10,11,12]
+y = [11,14,12,9,15,17]
+
+#정의
+plt.plot(x,y)
+
+#표시
+plt.show
